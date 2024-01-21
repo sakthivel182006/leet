558c4de32cf8d6c500bf98d97b5d60a972517c7f
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2024-01-21 17:01:33 +0530

    leetcode20
    leetcode 20 answer

---
+++ LEETCODE20.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') stk.push(c);
+            else if (stk.empty() || (c == ')' && stk.top() != '(') || (c == '}' && stk.top() != '{')
+                || (c == ']' && stk.top() != '[')) return false;
+            else stk.pop();
+        }
+        return stk.empty();
+    }
+};
